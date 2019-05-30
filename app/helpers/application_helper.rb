72c95d1b7c4b9a64a18d20cf59fe01dd1118967c
Sabrina <sabrina.muniz@icloud.com>
2019-05-30 11:11:45 -0400

    added class and id form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,27 @@ end
 
 def people_form_action(person)
     if person.new_record?
-        @action = "/people/new"
+        @action = "/people"
     else 
-        @action = "/people/person.id"
+        @action = "/people/#{@person.id}"
     end 
     @action
+end 
+
+def people_form_class(person)
+  if person.new_record?
+    @class = "/people/new"
+  else 
+    @class = "people/#{@person.id}"
+  end 
+  @class 
+end 
+
+def people_form_id(person)
+  if person.new_record?
+    @id = "new-person"
+  else 
+    @id = "#{@person.id}"
+  end 
+  @id
 end 
